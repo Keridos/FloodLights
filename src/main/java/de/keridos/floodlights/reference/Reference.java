9b27e4658de8ab68ccd3e8b6f4228da8a794ece0
Keridos <fuffzehn@gmail.com>
2014-12-02 23:25:28 +0100

    pushed version to 0.3.3

--- src/main/java/de/keridos/floodlights/reference/Reference.java
+++ src/main/java/de/keridos/floodlights/reference/Reference.java
@@ -7,7 +7,7 @@ package de.keridos.floodlights.reference;
 public class Reference {
     public static final String MOD_ID = "FloodLights";
     public static final String MOD_NAME = "Flood Lights";
-    public static final String VERSION = "0.3.2";
+    public static final String VERSION = "0.3.3";
     public static final String CHANNEL = "FloodLights";
     public static final String PROXY_LOCATION = "de.keridos.floodlights.core.proxy";
     public static final String DEPENDENCIES = "after:IC2";
