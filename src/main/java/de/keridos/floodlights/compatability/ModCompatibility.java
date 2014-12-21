702565a265f2b34f38ad017c7b6d30930067c5be
Keridos <fuffzehn@gmail.com>
2014-12-21 22:54:12 +0100

    derped last commit, now it starts properly on server again.

--- src/main/java/de/keridos/floodlights/compatability/ModCompatibility.java
+++ src/main/java/de/keridos/floodlights/compatability/ModCompatibility.java
@@ -33,8 +33,5 @@ public class ModCompatibility {
         BCLoaded = ModAPIManager.INSTANCE.hasAPI("BuildCraftAPI|tools");
         CofhCoreLoaded = ModAPIManager.INSTANCE.hasAPI("CoFHAPI|item");
         new IGWSupportNotifier();
-        if (IGWModLoaded) {
-            igwHandler = IGWHandler.getInstance();
-        }
     }
 }
