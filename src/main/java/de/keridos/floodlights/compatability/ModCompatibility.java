fddfc1eb70b4604cf296ff2fe5b896f04e92147c
Keridos <fuffzehn@gmail.com>
2015-06-20 01:28:33 +0200

    fixing build numbers in VersionChecker

--- src/main/java/de/keridos/floodlights/compatability/ModCompatibility.java
+++ src/main/java/de/keridos/floodlights/compatability/ModCompatibility.java
@@ -2,15 +2,12 @@ package de.keridos.floodlights.compatability;
 
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.ModAPIManager;
-import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.Optional;
 import cpw.mods.fml.common.event.FMLInterModComms;
 import de.keridos.floodlights.init.ModBlocks;
 import de.keridos.floodlights.reference.Reference;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-
-import java.util.Map;
 
 import static codechicken.nei.api.API.hideItem;
 
@@ -58,21 +55,12 @@ public class ModCompatibility {
         }
     }
 
-    public String getBuildNumber() {
-        Map<String, ModContainer> modList = Loader.instance().getIndexedModList();
-        if (modList.get(Reference.MOD_ID).getProcessedVersion().getVersionString().split("-").length > 0) {
-            return modList.get(Reference.MOD_ID).getProcessedVersion().getVersionString().split("-")[1];
-        } else {
-            return "0";
-        }
-    }
-
     private void addVersionCheckerInfo() {
         NBTTagCompound versionchecker = new NBTTagCompound();
         versionchecker.setString("curseProjectName", "224728-floodlights");
         versionchecker.setString("curseFilenameParser", "FloodLights-1.7.10-[]");
         versionchecker.setString("modDisplayName", "FloodLights");
-        versionchecker.setString("oldVersion", Reference.VERSION + "-" + getBuildNumber());
+        versionchecker.setString("oldVersion", Reference.VERSION);
         FMLInterModComms.sendRuntimeMessage("floodlights", "VersionChecker", "addCurseCheck", versionchecker);
     }
 
