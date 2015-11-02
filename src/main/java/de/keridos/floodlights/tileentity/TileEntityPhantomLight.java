5795afac0b80067e030cf574464d4b4c1e1345d8
Keridos <fuffzehn@gmail.com>
2015-11-02 16:19:14 +0100

    fixing a bug that had phantomlights be buggy on world load

--- src/main/java/de/keridos/floodlights/tileentity/TileEntityPhantomLight.java
+++ src/main/java/de/keridos/floodlights/tileentity/TileEntityPhantomLight.java
@@ -64,7 +64,7 @@ public class TileEntityPhantomLight extends TileEntity {
     public void readFromNBT(NBTTagCompound nbtTagCompound) {
         super.readFromNBT(nbtTagCompound);
         if (nbtTagCompound.hasKey(Names.NBT.SOURCES)) {
-            NBTTagList list = nbtTagCompound.getTagList(Names.NBT.ITEMS, Constants.NBT.TAG_INT_ARRAY);
+            NBTTagList list = nbtTagCompound.getTagList(Names.NBT.SOURCES, Constants.NBT.TAG_INT_ARRAY);
             for (int i = 0; i < list.tagCount(); i++) {
                 sources.add(list.func_150306_c(i));
             }
